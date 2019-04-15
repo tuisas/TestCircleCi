f2692f124ebe176bd0f3bbd2d1f860acc18bba1f
tuiagx <alejandro.gavilanes@tui.co.uk>
2019-04-15 14:17:52 +0100

    gulp test comment out code

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ gulp.task('assets', function() {
   return gulp.src('./app/assets/**/*').pipe(gulp.dest('./dist/assets/'));
 });
 
-gulp.task('scripts', function() {
+/*gulp.task('scripts', function() {
   return gulp
     .src('app/js/app.js')
 		.pipe( plumber( errorHandler ) )
@@ -50,21 +50,21 @@ gulp.task('html', function() {
     //.pipe(headerfooter.header('./app/partials/header.html'))
     //.pipe(headerfooter.footer('./app/partials/footer.html'))
     .pipe(gulp.dest('./dist/'));
-});
+});*/
 
-gulp.task('serve', gulp.series('sass', 'html', 'scripts', 'assets', function() {
+gulp.task('serve', gulp.series(/*'sass', 'html', 'scripts',*/ 'assets', function() {
   browserSync.init({
     server: './dist',
     open: false // set to false to disable browser autostart
   });
-  gulp.watch('app/scss/**/*', gulp.series('sass'));
-  gulp.watch('app/content/*.html',  gulp.series('html'));
+  //gulp.watch('app/scss/**/*', gulp.series('sass'));
+  //gulp.watch('app/content/*.html',  gulp.series('html'));
   //gulp.watch('app/partials/*.html', gulp.series('html'));
-  gulp.watch('app/js/*.js',  gulp.series('scripts'));
+  //gulp.watch('app/js/*.js',  gulp.series('scripts'));
   gulp.watch('app/assets/**/*', gulp.series('assets'));
-  gulp.watch('dist/*.html').on('change', browserSync.reload);
-  gulp.watch('dist/js/*.js').on('change', browserSync.reload);
+  //gulp.watch('dist/*.html').on('change', browserSync.reload);
+  //gulp.watch('dist/js/*.js').on('change', browserSync.reload);
 }));
 
-gulp.task('build', gulp.series('sass', 'html' ,'scripts', 'assets'));
+gulp.task('build', gulp.series(/*'sass', 'html' ,'scripts',*/ 'assets'));
 gulp.task('default', gulp.series('serve'));
