83a60c5eea5a0c210589db9f13bf8ccbe4493872
Alejandro Gavilanes <buunney69@hotmail.co.uk>
2019-04-16 21:49:15 +0100

    revert deploy file

--- .circleci/deploy.js
+++ .circleci/deploy.js
@@ -1,59 +1,17 @@
-const fs = require('fs');
-const FtpClient = require('ftp');
-const glob = require('glob');
-
-const basePath = './dist';
-const destinationPath = '/htdocs/AlexTest/';
-const config = {
-    // We store the credentials for
-    // our FTP server as environemnt
-    // variables for security reasons.
-    host: process.env.FTP_HOST,
-    password: process.env.FTP_PASSWORD,
-    user: process.env.FTP_USER,
-};
-
-const ftpClient = new FtpClient();
-
-function createDirectory(destination) {
-    return ftpClient.mkdir(destination, true, (error) => {
-        if (error) throw error;
-
-        ftpClient.end();
-    });
+var FtpDeploy = require('ftp-deploy');
+var ftpDeploy = new FtpDeploy();
+ 
+var config = {
+    username: process.env.FTPUSERNAME,
+    password: process.env.FTPPASS,
+    host: process.env.FTPHOST,
+    port: 21,
+    localRoot: __dirname + "/../dist/",
+    remoteRoot: "/",
+    include: ['*']
 }
-
-function uploadFile(file, destination) {
-    ftpClient.put(file, destination, (error) => {
-        if (error) throw error;
-
-        console.log(`${file} => ${destination}`);
-        ftpClient.end();
-    });
-}
-
-// Check if the path is a directory and
-// either create the directory on the server
-// if it is a directory, or upload the file
-// if it is a file.
-function handlePath(path) {
-    const relativeFile = path.replace(`${basePath}/`, '');
-    const destination = `${destinationPath}/${relativeFile}`;
-
-    if (fs.lstatSync(path).isDirectory()) {
-        return createDirectory(destination);
-    }
-
-    return uploadFile(path, destination);
-}
-
-ftpClient.on('ready', () => {
-    // Get an array of all files and directories
-    // in the given base path and send them to the
-    // `handlePath()` function to decide if a
-    // directory is created on the server or the
-    // file is uploaded.
-    glob.sync(`${basePath}/**/*`).forEach(handlePath);
+    
+ftpDeploy.deploy(config, function(err) {
+    if (err) console.log(err)
+    else console.log('finished');
 });
-
-ftpClient.connect(config);
