6924e9fd01f17f2f524b15f70bd77e12b67ca9ba
Alejandro Gavilanes <buunney69@hotmail.co.uk>
2019-04-17 21:46:46 +0100

    new gulp code

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,6 @@
 var gulp = require('gulp')
-var merge = require('merge-stream');
-
 gulp.task('copy', function() {
-	var allStreams = [
-		gulp.src('./app/assets/**/*').pipe(gulp.dest('./dist/assets/')),
-		gulp.src('./app/content/*.html').pipe(gulp.dest('./dist/')),
-	];
-	return merge.apply(this, allStreams);
+	return gulp.src(['./app/assets/**/*']).pipe(gulp.dest('./dist/assets/')) 
 });
-gulp.task('build', [copy], function(){
-
+gulp.task('compress', [copy], function(){
 });
-
-
-
-
